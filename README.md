cb82de0a01b31a7c1c01c5edbe1a6451871cc31f
hoangphu7122002 <phu.nguyen7122002hp@hcmut.edu.vn>
2023-12-03 23:47:09 +0700

    final version

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# vie-flight
-vie-flight is a reply system about Vietnamese flights's information using natural language techniques.
+RUN python main.py
--- test1.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-from underthesea import pos_tag
-print(pos_tag('Máy bay nào bay từ Đà Nẵng đến Hồ Chí Minh mất một giờ'))
-
-print(pos_tag(' '.join(['máy_bay', 'vn4', 'có', 'xuất_phát', 'từ', 'đà_nẵng', 'không', '?'])))
